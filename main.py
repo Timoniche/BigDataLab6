36fcbf46436873d389cb9040e832cfaa4da442f4
timoniche <dmitriidulaev@gmail.com>
2024-05-22 18:39:53 +0300

    minor: k cycle removed, collect added, spark config extended

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def main():
 
     vectorized_dataset = vectorizer.vectorize(dataset)
     scaled_dataset = scaler.scale(vectorized_dataset)
+    scaled_dataset.collect()
 
     scores = clusterizer.clusterize(scaled_dataset)
     plot_silhouette_scores(scores, clusterizer.k_search_range)
--- spark_session_provider.py
+++ spark_session_provider.py
@@ -16,6 +16,8 @@ class SparkSessionProvider:
             .config("spark.executor.memory", self.config['spark']['executor.memory']) \
             .config("spark.driver.cores", self.config['spark']['driver.cores']) \
             .config("spark.executor.cores", self.config['spark']['executor.cores']) \
+            .config("spark.driver.host", "127.0.0.1") \
+            .config("spark.driver.bindAddress", "127.0.0.1") \
             .getOrCreate()
 
     def provide_session(self) -> SparkSession:
